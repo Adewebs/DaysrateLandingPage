b225342252c5efa3a1059bf5512db2a5e0467a69
adewebstech <adewebstech@gmail.com>
2025-03-08 21:47:23 +0100

    updated send email verificaiton

--- auth_manager/emailverification.py
+++ auth_manager/emailverification.py
@@ -10,12 +10,18 @@ class EmailVerificationSerializer(serializers.Serializer):
     email = serializers.EmailField()
     token = serializers.CharField()
 
+
 class EmailVerificationView(generics.GenericAPIView):
     """Verify the registration token from the email verification request"""
     serializer_class = EmailVerificationSerializer
 
     def post(self, request, *args, **kwargs):
+        # Check if the request data is valid
+        if not request.data:
+            return Response({"detail": "No data provided."}, status=status.HTTP_400_BAD_REQUEST)
+
         serializer = EmailVerificationSerializer(data=request.data)
+
         if serializer.is_valid():
             email = serializer.validated_data['email']
             token = serializer.validated_data['token']
@@ -33,5 +39,7 @@ class EmailVerificationView(generics.GenericAPIView):
                 return Response({"detail": "Email successfully verified."}, status=status.HTTP_200_OK)
             else:
                 return Response({"detail": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # If the serializer is not valid, return errors
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
